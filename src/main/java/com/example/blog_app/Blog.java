3e1788d6bd4716b594521bcbfbba81fca3df11cc
Fuchikami <2026031@fsc-educ.com>
2026-06-12 15:16:18 +0900

    最終

---
+++ src/main/java/com/example/blog_app/Blog.java
@@ -0,0 +1,27 @@
+package com.example.blog_app;
+
+public class Blog {
+    private final long id;
+    private final String title;
+
+    private final String notes;
+
+    public Blog(long id, String title, String notes) {
+        this.id = id;
+        this.title = title;
+        this.notes = notes;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+}
--- src/main/java/com/example/blog_app/BlogController.java
+++ src/main/java/com/example/blog_app/BlogController.java
@@ -1,14 +1,51 @@
 package com.example.blog_app;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BlogController {
-    @GetMapping("/blogs")
-    public String blog() {
-        return "blogs";
+    private final BlogRepository blogRepository;
+
+    public BlogController(BlogRepository blogRepository) {
+        this.blogRepository = blogRepository;
     }
-    
+
+    @GetMapping("/all")
+    public String blogs(Model model) {
+        model.addAttribute("blogs", blogRepository.findAll());
+        return "All";
+    }
+
+    @GetMapping("/blog")
+    public String blog() {
+        return "blog";
+    }
+
+    @GetMapping("/new")
+    public String newBlog() {
+        return "new";
+    }
+
+    @GetMapping("/blogs/{id}")
+    public String detail(@PathVariable Long id, Model model) {
+        Optional<Blog> blogOpt = blogRepository.findById(id);
+        if (blogOpt.isEmpty()) {
+            return "redirect:/blogss";
+        }
+        model.addAttribute("blog", blogOpt.get());
+        return "detail";
+    }
+
+    @PostMapping("/blogs")
+    public String create(@ModelAttribute BlogForm form) {
+        blogRepository.save(form);
+        return "redirect:/all";
+    }
+
 }
